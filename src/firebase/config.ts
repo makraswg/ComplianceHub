b835110e514d84be18c5af5d7314f204dd0c389e
SWG mbH Nordhausen <swg.nordhausen@gmail.com>
2026-02-02 21:07:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2420462182-df9be",
+  "appId": "1:174720195864:web:fcdf76b27fe704d964926d",
+  "apiKey": "AIzaSyCPfIrYMWrCLwUO2jrk-uz8AnjklwtphBM",
+  "authDomain": "studio-2420462182-df9be.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "174720195864"
+};
