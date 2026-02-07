b510b75ccf7ae1539d2505bcaa649cb2e9831cdc
SWG mbH Nordhausen <swg.nordhausen@gmail.com>
2026-02-07 18:27:14 +0000

    Try fixing this error: `Runtime ReferenceError: RefreshCw is not defined

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -32,7 +32,8 @@ import {
   Sparkles,
   BookOpen,
   Mail,
-  FileCode
+  FileCode,
+  RefreshCw
 } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
